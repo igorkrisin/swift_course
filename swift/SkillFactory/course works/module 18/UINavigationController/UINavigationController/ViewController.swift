cf23c73d3ac1893d407233a0d281376f1a5f2540
Игорь Крысин <igor_krisin@mail.ru>
2023-12-27 20:37:51 +0300

    clear to folder swift 3

---
+++ swift/SkillFactory/course works/module 18/UINavigationController/UINavigationController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UINavigationController
+//
+//  Created by Игорь Крысин on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
