19ee5b63dd5115e590ec5ddd64f92387b29c66af
sheogorat <igor_krisin@mail.ru>
2024-01-12 22:38:09 +0300

    module 20 applicationSmileTemperature  is done

--- swift/SkillFactory/course works/module 20/TemperatureSmileCollectionApp/TemperatureSmileCollectionApp/View/OneViewController.swift
+++ swift/SkillFactory/course works/module 20/TemperatureSmileCollectionApp/TemperatureSmileCollectionApp/View/OneViewController.swift
@@ -11,22 +11,47 @@ class OneViewController: UIViewController, UICollectionViewDelegate, UICollectio
     
     var arrayImageName: [String] = ["temp.blackGreen", "temp.darkYellow", "temp.green", "temp.lightYellow", "temp.orange", "temp.red"]
     
+    var arraySmile: [String] = ["favorite", "nice", "notPleasant", "routine", "bad", "hate"]
+    
     @IBOutlet weak var collectionViewOne: UICollectionView!
+    
+    @IBOutlet weak var collectionViewTwo: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionViewOne.dataSource = self
         collectionViewOne.delegate = self
+        
+        
+        collectionViewTwo.dataSource = self
+        collectionViewTwo.delegate = self
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return arrayImageName.count
+        if collectionView == collectionViewOne {
+            return arrayImageName.count
+        }
+        if collectionView == collectionViewTwo {
+            return arraySmile.count
+        }
+        
+        return 0
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ViewCell {
-            cell.setTemperatureImage(name: arrayImageName[indexPath.row])
-            return cell
+        if collectionView == collectionViewOne {
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? ViewCell {
+                cell.setTemperatureImage(name: arrayImageName[indexPath.row])
+                return cell
+            }
+        }
+        if collectionView == collectionViewTwo  {
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellTwo", for: indexPath) as? ViewCell {
+                cell.setSmileImage(name: arraySmile[indexPath.row])
+                return cell
+            }
         }
         
         return UICollectionViewCell()
@@ -36,9 +61,16 @@ class OneViewController: UIViewController, UICollectionViewDelegate, UICollectio
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "ShowImageVC") as? SmileViewController else { return }
         
-        var currentSelectedImage = arrayImageName[indexPath.row]
-        vc.setImageName(name: currentSelectedImage)
-        present(vc, animated: true, completion: nil)
+ 
+            
+            var currentSelectedImage = arrayImageName[indexPath.row]
+        if collectionView == collectionViewOne {
+            vc.setImageName(name: currentSelectedImage)
+        } else {
+            vc.setImageName(name: arraySmile[indexPath.row])
+        }
+            present(vc, animated: true, completion: nil)
+        
     }
 }
 
--- swift/SkillFactory/course works/module 20/TemperatureSmileCollectionApp/TemperatureSmileCollectionApp/View/ViewCell.swift
+++ swift/SkillFactory/course works/module 20/TemperatureSmileCollectionApp/TemperatureSmileCollectionApp/View/ViewCell.swift
@@ -12,8 +12,15 @@ class ViewCell: UICollectionViewCell {
     
     @IBOutlet weak var temperatureImage: UIImageView!
     
+    @IBOutlet weak var smileImage: UIImageView!
+    
+    
     func setTemperatureImage(name: String) {
         self.temperatureImage.image = UIImage(named: name)
+    }
+    
+    func setSmileImage(name:String) {
+        self.smileImage.image = UIImage(named: name)
     }
     
     
