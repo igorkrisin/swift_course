114871787aa11e06dcf0e4c39bb02ae2881b6f71
Igor Krysin <igor_krisin@mail.ru>
2024-02-09 20:27:45 +0300

    test

---
+++ swift/SkillFactory/home works/TodoMVC/TodoMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoMVC
+//
+//  Created by Игорь Крысин on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
